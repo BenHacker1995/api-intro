6da24eb63f386d53b5ecab37a55a08fd26cef62b
Ben Hacker <bhacker1995@gmail.com>
2018-06-05 10:11:32 -0500

    set up api

--- server/public/client.js
+++ server/public/client.js
@@ -3,6 +3,15 @@ const app = angular.module( 'apiApp', [] );
 app.controller( 'apiController', [ '$http', function( $http ) {
     let vm = this;
     vm.welcome = "Welcome to my awesome SWAPI app!!!";
+    vm.person = {};
     
-    
+    $http({
+        method: 'GET',
+        url: 'https://swapi.co/api/people/10'
+    }).then( function( response ) {
+        vm.person = response.data;
+        console.log( 'Response from SWAPI ', response.data );
+    }).catch( function( error ) {
+        console.log( `Oh no! Error ${ error } occurred` );
+    })
 }]);
